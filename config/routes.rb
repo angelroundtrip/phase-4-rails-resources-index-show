342b1037ca625349c5017529df40a1e98325b1c1
Angel Rosario <arosario.us@gmail.com>
2022-03-14 17:44:45 -0400

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #! Does the same job as code that's commented out
+  resources :birds, only: [:index, :show]
+  
+  # get '/birds', to: 'birds#index'
+  # get '/birds/:id', to: 'birds#show'
 end
